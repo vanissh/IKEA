ddc7466cdf34be898baad903da3649f4cd676484
Anna Kuznetsova <sutcliff0684@gmail.com>
2021-03-17 04:58:26 +0300

    data submit

--- src/modules/generateCartPage.js
+++ src/modules/generateCartPage.js
@@ -1,6 +1,45 @@
 import getData from './getData.js';
 import userData from './userData.js';
 
+const sendData = async(url, data) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: data, 
+    });
+
+    if(!response.ok){
+        throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
+    }
+
+    return await response.json();
+};
+
+const sendCart = () => {
+    
+    const cartForm = document.querySelector('.cart-form');
+
+    cartForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const formData = new FormData(cartForm);
+        const data = {};
+
+        for (const [key, value] of formData){
+            data[key] = value;
+        }
+
+        data.order = userData.cartList;
+
+        sendData('', JSON.stringify(data))
+            .then(() => {
+                cartForm.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    })
+}
+
 const generateCartPage = () => {
 
     if(location.pathname.includes('cart')){
@@ -73,8 +112,6 @@ const generateCartPage = () => {
 
         };
 
-        
-
         cartList.addEventListener('change', e => {
             userData.changeCount = {
                 id: e.target.dataset.idd,
@@ -94,6 +131,8 @@ const generateCartPage = () => {
         })
         
         getData.cart(userData.cartList, renderCartList);
+
+        sendCart();
     }
 };
 
